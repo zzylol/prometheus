9e607362db220a5d1a99fa4fb875ac55bb57b727
Zeying Zhu <zeyingz@bu.edu>
2022-09-12 21:40:10 -0400

    add entropy profile

--- rules/recording_test.go
+++ rules/recording_test.go
@@ -114,9 +114,12 @@ func TestRuleEvalPressure(t *testing.T) {
 	err = suite.Run() // just load data 
 	require.NoError(t, err)
 
-	// quantile
-	expr, err := parser.ParseExpr(`sum by(instance) (quantile_over_time(0.99, http_requests[900s]))`)
+	// quantile_over_time (range query)
+	// expr, err := parser.ParseExpr(`sum by(instance) (quantile_over_time(0.99, http_requests[900s]))`)
+	// avg_over_time (range query)
 	// expr, err := parser.ParseExpr(`sum by(instance) (avg_over_time(http_requests[900s]))`)
+	// Entropy (instant query)
+	expr, err := parser.ParseExpr(`sum by(instance) (sum(http_requests * ln(http_requests)))`)
 	require.NoError(t, err)
 
 
